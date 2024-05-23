bc2871938ef867d0c7fcf890e8b401f8e9afdc83
onebeany <wonbin0221@gmail.com>
2024-05-23 18:04:39 +0900

    Update Visualization

--- visualization/inflectionAndTrejectory.py
+++ visualization/inflectionAndTrejectory.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
 import argparse
 
-def main(inputfile, compare):
+def main(inputfile, compare1, compare2):
     data = pd.read_csv(inputfile)
     x = data['x_m'].values
     y = data['y_m'].values
@@ -39,26 +39,42 @@ def main(inputfile, compare):
     for i in range(len(inflection_points)):
         print(f"x: {inflection_x[i]}, y: {inflection_y[i]}")
 
-    data1 = pd.read_csv(compare)
-    xc = data1['x'].values
-    yc = data1['y'].values
+    data1 = pd.read_csv(compare1)
+    xc1 = data1['x'].values
+    yc1 = data1['y'].values
 
-    tree = KDTree(np.c_[xc, yc])
-    distances, indices = tree.query(np.c_[inflection_x, inflection_y])
+    tree = KDTree(np.c_[xc1, yc1])
+    distances, indices1 = tree.query(np.c_[inflection_x, inflection_y])
 
-    inflection_xc = xc[indices]
-    inflection_yc = yc[indices]
+    inflection_xc1 = xc1[indices1]
+    inflection_yc1 = yc1[indices1]
+
+    data2 = pd.read_csv(compare2)
+    xc2 = data2['x'].values
+    yc2 = data2['y'].values
+
+    tree = KDTree(np.c_[xc2, yc2])
+    distances, indices2 = tree.query(np.c_[inflection_x, inflection_y])
+
+    inflection_xc2 = xc2[indices2]
+    inflection_yc2 = yc2[indices2]
 
     print("Inflection Points of compare file (trajectory, wayoints, etc):")
-    for i in range(len(indices)):
-        print(f"x: {inflection_xc[i]}, y: {inflection_yc[i]}")
+    for i in range(len(indices1)):
+        print(f"x: {inflection_xc1[i]}, y: {inflection_yc1[i]}")
+
+    print("Inflection Points of compare file (trajectory, wayoints, etc):")
+    for i in range(len(indices2)):
+        print(f"x: {inflection_xc2[i]}, y: {inflection_yc2[i]}")
 
     plt.figure(figsize=(10, 6))
     plt.plot(x, y, 'o-', label='Middle Line', markersize=3)
-    plt.plot(xc, yc, 'o-', label='Trajectory', markersize=3)
-    plt.plot(x_half, y_half, 'o-', label='Selected Data', markersize=3)
+    plt.plot(xc1, yc1, 'o-', label='Trajectory1', markersize=3)
+    plt.plot(xc2, yc2, 'o-', label='Trajectory2', markersize=3)
+    # plt.plot(x_half, y_half, 'o-', label='Selected Data', markersize=3)
     plt.plot(inflection_x, inflection_y, 'rx', label='Inflection Points (Middle Line)', markersize=8)
-    plt.plot(inflection_xc, inflection_yc, 'bx', label='Inflection Points (For compare)', markersize=8)
+    plt.plot(inflection_xc1, inflection_yc1, 'bx', label='Inflection Points (Trajectory 1)', markersize=8)
+    plt.plot(inflection_xc2, inflection_yc2, 'cx', label='Inflection Points (Trajectory 2)', markersize=8)
     plt.xlabel('X Position')
     plt.ylabel('Y Position')
     plt.title('Trajectory with Inflection Points')
@@ -69,7 +85,8 @@ def main(inputfile, compare):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find and save inflection points from trajectory data.')
     parser.add_argument('inputfile', type=str, help='Path to the input CSV file containing middle line data.')
-    parser.add_argument('compare', type=str, help='Path to the input CSV file containing anoter data.')
+    parser.add_argument('compare1', type=str, help='Path to the input CSV file containing anoter data.')
+    parser.add_argument('compare2', type=str, help='Path to the input CSV file containing anoter data.')
     args = parser.parse_args()
     
-    main(args.inputfile)
+    main(args.inputfile, args.compare1, args.compare2)
---
+++ visualization/viewTrajectories.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def main(file1, file2, file3, file4):
+    
+    data1 = np.genfromtxt(file1, delimiter=',', skip_header=1)  # 헤더가 있다면 skip_header=1을 사용
+    x1 = data1[:, 1] 
+    y1 = data1[:, 2] 
+
+    data2 = np.genfromtxt(file2, delimiter=',', skip_header=1)
+    x2 = data2[:, 1] 
+    y2 = data2[:, 2] 
+
+    data3 = np.genfromtxt(file3, delimiter=',', skip_header=1)
+    x3 = data3[:, 1] 
+    y3 = data3[:, 2] 
+
+    data4 = np.genfromtxt(file4, delimiter=',', skip_header=1)
+    x4 = data4[:, 1] 
+    y4 = data4[:, 2] 
+    
+    
+    # 플롯 생성
+    plt.figure(figsize=(20, 15))  # 그래프 크기 설정
+    
+    plt.plot(x1, y1, 'o-', label='Trajectory1', markersize=3)  # 투명도 설정하여 겹치는 부분을 줄임
+    plt.plot(x2, y2, 'o-', label='Trajectory2', markersize=3)
+    plt.plot(x3, y3, 'o-', label='Trajectory3', markersize=3)
+    plt.plot(x4, y4, 'o-', label='Trajectory4', markersize=3)
+    
+    plt.title('Trajectory Visualization')  # 그래프 제목
+    plt.xlabel('X Position')  # x축 레이블
+    plt.ylabel('Y Position')  # y축 레이블
+    plt.legend()
+    plt.grid(True)  # 그리드 표시
+    plt.show()  # 그래프 표시
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 5:
+        print("Usage: python3 viewTrajectories.py file1.csv file2.csv file3.csv file4.csv")
+        sys.exit(1)
+    
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+    file3 = sys.argv[3]
+    file4 = sys.argv[4]
+    
+    main(file1, file2, file3, file4)
